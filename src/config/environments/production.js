6d74e59c5784a8a079b3577ad7074945648643b4
ankitsangani <ankitsangani786@gmail.com>
2021-03-23 23:09:57 +0530

    new

--- src/config/environments/production.js
+++ src/config/environments/production.js
@@ -1,7 +1,7 @@
 module.exports = {
     host: "localhost",
     port: 9999,
-    mongoUrl: "mongodb://localhost:27017/demo",
+    mongoUrl: "mongodb+srv://jeelbhanvadiya:mrjay1831301@ecarrenting-h1uct.mongodb.net/client?retryWrites=true&w=majority",
     logLevel: "debug",
     secret: "superSuperSecret",
 };
