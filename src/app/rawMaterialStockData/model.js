4df390d3ccfe8b62b634ee3f43406d60154ee631
ankitsangani <ankitsangani786@gmail.com>
2021-04-05 21:13:33 +0530

    changes in modal

--- src/app/rawMaterialStockData/model.js
+++ src/app/rawMaterialStockData/model.js
@@ -6,7 +6,7 @@ const rawMaterialStockSchema = new Schema(
     {
         typeId: {type: Number, required: true},
         typeName: {type: String, required: true},
-        gasType: {type: String, required: true},
+        gasType: {type: String, required: false},
         weight: {type: Number, required: false},
         piece: {type: Number, required: false},
         billNo: {type: String, required: false},
