adaa26141d3005c80236b10f48559d1313a042ba
ankitsangani <ankitsangani786@gmail.com>
2021-05-07 14:19:29 +0530

    changes

--- src/app/users/model.js
+++ src/app/users/model.js
@@ -8,8 +8,9 @@ const userSchema = new Schema( {
     firstName: {type: String, required: false},
     middleName: {type: String, required: false},
     lastName: {type: String, required: false},
-    email: {type: String, required: false},
-    password: {type: String, required: false},
+    active: {type:Boolean,required:false,default: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
     phoneNumber: {type: Number, required: false},
     GSTNo: {type: String, required: false},
     address: {
