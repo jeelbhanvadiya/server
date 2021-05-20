adc09847f9d40fb1214eed65bac6bc49abf2529b
ankitsangani <ankitsangani786@gmail.com>
2021-05-20 09:39:41 +0530

    added field in service

--- src/app/services/model.js
+++ src/app/services/model.js
@@ -15,7 +15,8 @@ const servicesSchema = new Schema(
                 serviceDate : {type: Date,default : null},
                 completeDate : {type: Date,default : null},
                 serviceRating: {type:Number, default:0},
-                serviceBoyRating : {type:Number ,default : 0}
+                serviceBoyRating : {type:Number ,default : 0},
+                remarks : {type:String ,required : false},
             }
         ],
     },
