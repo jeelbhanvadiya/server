357e93a51aa7e6ecd2ffef8f2e68264d690dcac3
Mukund-Khunt <64716112+Mukundkhunt@users.noreply.github.com>
2022-11-01 19:10:35 +0530

    current stock data count

--- src/app/home/controller.js
+++ src/app/home/controller.js
@@ -9,11 +9,10 @@ const acData = require('../accData/model')
 exports.homePage = async (req, res) => {
     try {
         const serviceData = await Services.find({});
-        const stockData = await Stock.find({});
         const rawMaterialData = await rawMaterial.find({});
         const attendanceData = await attendance.find({});
         const ac_data = await acData.find({});
-        res.status(200).json({ status: 200, message: "Home page successfully!", data: { serviceData, stockData, rawMaterialData, attendanceData, ac_data }, error: {} });
+        res.status(200).json({ status: 200, message: "Home page successfully!", data: { serviceData, rawMaterialData, attendanceData, ac_data }, error: {} });
     } catch (err) {
         res.status(500).send({ message: err.message || "Some error occurred while retrieving login." });
     }
@@ -23,7 +22,8 @@ exports.homePageCount = async (req, res) => {
     try {
         const sellStockData = await Stock.countDocuments({ sell: true });
         const deleteStockData = await Stock.countDocuments({ isDeleted: true });
-        res.status(200).json({ status: 200, message: "Home page count successfully!", data: { sellStockData, deleteStockData }, error: {} });
+        const currentStockData = await Stock.countDocuments({ isDeleted: false, sell: false });
+        res.status(200).json({ status: 200, message: "Home page count successfully!", data: { sellStockData, deleteStockData, currentStockData }, error: {} });
     } catch (err) {
         res.status(500).send({ message: err.message || "Some error occurred while retrieving login." });
     }
