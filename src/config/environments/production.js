587151def4464f73c4d16fbecd37b4843ea5c7a1
Mukund-Khunt <64716112+Mukundkhunt@users.noreply.github.com>
2022-11-02 19:32:30 +0530

    database switching

--- src/config/environments/production.js
+++ src/config/environments/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: "localhost",
-    mongoUrl: "mongodb+srv://JKRAdmin:Rnbk8u2UvXs0b5Ui@jkr.tek1e.mongodb.net/Jkr-admin?retryWrites=true&w=majority",
+    mongoUrl: "mongodb+srv://nodejs:9nuDETxvDexCJYZY@nodejs.nqle14b.mongodb.net/jkr_production?retryWrites=true&w=majority",
     logLevel: "debug",
     secret: "superSuperSecret",
 };
--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || "production";
 console.log(env);
 
-const config = require( `./environments/${ env.toLowerCase( ) }` );
+const config = require(`./environments/${env.toLowerCase()}`);
 
 module.exports = config;
