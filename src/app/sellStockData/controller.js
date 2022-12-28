f5685c2a7d55d39bf72bca18733c833c6a75b0ee
Mukund-Khunt <121028068+baldhapratik@users.noreply.github.com>
2022-12-28 15:42:09 +0530

    fix sell stock

--- src/app/sellStockData/controller.js
+++ src/app/sellStockData/controller.js
@@ -13,9 +13,9 @@ function getCurrentYear() {
 
 exports.createSellStock = async (req, res) => {
     try {
-        const {clientPhoneNo, stock} = req.body;
+        const { clientPhoneNo, stock } = req.body;
         const year = getCurrentYear();
-        const data = await sellStock.aggregate([{$count: "sellStock"}]);
+        const data = await sellStock.aggregate([{ $count: "sellStock" }]);
         let count = 1;
         if (data && data.length > 0 && data[0].sellStock) {
             count = data[0].sellStock + 1
@@ -23,57 +23,40 @@ exports.createSellStock = async (req, res) => {
         stock.forEach(stock => {
             stock.billNo = `JKR/${(parseInt(year) - 1) + "-" + parseInt(year)}/GST${getNumber(count, 5)}`
         });
-        const isExist = await sellStock.findOne({clientPhoneNo: clientPhoneNo})
-        if (isExist && isExist._id) {
-            const update = await sellStock.updateOne({clientPhoneNo: clientPhoneNo}, {
-                $push: {
-                    stock: {
-                        $each: stock,
-                        $position: -2
-                    }
+        const isExist = await sellStock.findOne({ clientPhoneNo: clientPhoneNo })
+        const create = await sellStock.create(req.body)
+        if (create && create._id) {
+            const promiseBuilder = {
+                updateAppPromise: (payload) => {
+                    return new Promise(async (resolve) => {
+                        const update = await stockModal.updateOne({ stockNo: payload.stockNo }, {
+                            sell: true,
+                            clientPhoneNo: clientPhoneNo
+                        });
+                        if (update && update.ok) {
+                            return resolve({ success: true });
+                        } else {
+                            return resolve({ success: false });
+                        }
+                    });
+                }
+            };
+            const allPromises = [];
+            stock.forEach((item) => {
+                allPromises.push(promiseBuilder.updateAppPromise(item));
+            });
+            await Promise.all(allPromises).then(values => {
+                if (values.some(value => value.success)) {
+                    res.status(200).send(create)
+                } else {
+                    res.status(500).send({ success: false, message: "something went wrong" })
                 }
             });
-            if (update && update.ok) {
-                res.status(200).send({updated: true, message: "successfully updated"});
-            } else {
-                res.status(200).send({updated: false, message: "something went wrong"});
-            }
         } else {
-            const create = await sellStock.create(req.body)
-            if (create && create._id) {
-                const promiseBuilder = {
-                    updateAppPromise: (payload) => {
-                        return new Promise(async (resolve) => {
-                            const update = await stockModal.updateOne({stockNo: payload.stockNo}, {
-                                sell: true,
-                                clientPhoneNo: clientPhoneNo
-                            });
-                            if (update && update.ok) {
-                                return resolve({success: true});
-                            } else {
-                                return resolve({success: false});
-                            }
-                        });
-                    }
-                };
-                const allPromises = [];
-                stock.forEach((item) => {
-                    allPromises.push(promiseBuilder.updateAppPromise(item));
-                });
-                await Promise.all(allPromises).then(values => {
-                    if (values.some(value => value.success)) {
-                        res.status(200).send(create)
-                    } else {
-                        res.status(500).send({success: false, message: "something went wrong"})
-                    }
-                });
-            } else {
-                res.status(200).send({success: false, message: 'something went wrong'})
-            }
-
+            res.status(200).send({ success: false, message: 'something went wrong' })
         }
     } catch (err) {
-        res.status(500).send({message: err.message || "Some error occurred while creating sell -stock."});
+        res.status(500).send({ message: err.message || "Some error occurred while creating sell -stock." });
     }
 };
 
@@ -90,7 +73,7 @@ exports.getSellStock = async (req, res) => {
         const SellStock = await sellStock.find(query);
         res.status(200).send(SellStock);
     } catch (err) {
-        res.status(500).send({message: err.message || "Some error occurred while retrieving login."});
+        res.status(500).send({ message: err.message || "Some error occurred while retrieving login." });
     }
 };
 
@@ -107,7 +90,7 @@ exports.getSellStockStockNo = async (req, res) => {
             res.status(200).send("Not Found any data");
         }
     } catch (err) {
-        res.status(500).send({message: err.message || "Some error occurred while retrieving login."});
+        res.status(500).send({ message: err.message || "Some error occurred while retrieving login." });
     }
 };
 
@@ -121,7 +104,7 @@ exports.searchingSellStock = async (req, res) => {
             keyName = "stock.".concat(Object.keys(req.query)[0].toString())
         }
         if (req.query.clientPhoneNo) {
-            stockData = await sellStock.find({clientPhoneNo: Number(req.query.clientPhoneNo)})
+            stockData = await sellStock.find({ clientPhoneNo: Number(req.query.clientPhoneNo) })
         } else if (req.query.stockNo) {
             const updatedStockData = await sellStock.aggregate([
                 {
@@ -130,7 +113,7 @@ exports.searchingSellStock = async (req, res) => {
                             $filter: {
                                 input: "$stock",
                                 as: "item",
-                                cond: { $eq: [ "$$item.stockNo", req.query.stockNo ] }
+                                cond: { $eq: ["$$item.stockNo", req.query.stockNo] }
                             }
                         },
                         clientName: 1,
@@ -140,11 +123,11 @@ exports.searchingSellStock = async (req, res) => {
             ])
             stockData = updatedStockData.filter(stock => stock.stock.length > 0) || []
         } else {
-            stockData = await sellStock.find({[`${keyName}`]: {$regex: Object.values(req.query)[0], $options: 'i'}})
+            stockData = await sellStock.find({ [`${keyName}`]: { $regex: Object.values(req.query)[0], $options: 'i' } })
         }
         res.status(200).send(stockData)
     } catch (err) {
-        res.status(500).send({message: err.message || "Some error occurred while retrieving login."});
+        res.status(500).send({ message: err.message || "Some error occurred while retrieving login." });
     }
 };
 
@@ -152,7 +135,7 @@ exports.searchingSellStock = async (req, res) => {
 exports.updateSellStock = async (req, res) => {
     try {
         if (req.body.stockNo) {
-            const editedCompany = await sellStock.updateOne({"stock.stockNo": req.body.stockNo}, {
+            const editedCompany = await sellStock.updateOne({ "stock.stockNo": req.body.stockNo }, {
                 $set: {
                     "stock.$.CrmNo": req.body.CrmNo,
                     'stock.$.CustomerNo': req.body.CustomerNo,
@@ -160,13 +143,13 @@ exports.updateSellStock = async (req, res) => {
                 }
             });
             if (editedCompany && editedCompany.ok) {
-             await stockModal.updateOne({stockNo : req.body.stockNo},{$set : {crmStatus: true}})
-                res.status(200).send({updated: true});
+                await stockModal.updateOne({ stockNo: req.body.stockNo }, { $set: { crmStatus: true } })
+                res.status(200).send({ updated: true });
             } else {
-                res.status(200).send({updated: false, message: "something went wrong"});
+                res.status(200).send({ updated: false, message: "something went wrong" });
             }
         } else {
-            res.status(404).send({success: false, message: "Please send correct user info"});
+            res.status(404).send({ success: false, message: "Please send correct user info" });
         }
     } catch (err) {
         res.send(err);
@@ -176,14 +159,14 @@ exports.updateSellStock = async (req, res) => {
 exports.updateSellData = async (req, res) => {
     try {
         if (req.body && req.body.stock) {
-            const updated = await sellStock.updateOne({"stock.stockNo": req.body.stock.stockNo}, req.body);
+            const updated = await sellStock.updateOne({ "stock.stockNo": req.body.stock.stockNo }, req.body);
             if (updated && updated.ok) {
-                res.status(200).send({updated: true});
+                res.status(200).send({ updated: true });
             } else {
-                res.status(200).send({updated: false, message: "something went wrong"});
+                res.status(200).send({ updated: false, message: "something went wrong" });
             }
         } else {
-            res.status(404).send({success: false, message: "Please send correct data"});
+            res.status(404).send({ success: false, message: "Please send correct data" });
         }
     } catch (err) {
         res.send(err);
@@ -192,9 +175,9 @@ exports.updateSellData = async (req, res) => {
 
 exports.deleteSellStock = async (req, res) => {
     try {
-        await sellStock.deleteOne({_id: req.params.id})
+        await sellStock.deleteOne({ _id: req.params.id })
         res.status(200).send("success");
     } catch (err) {
-        res.status(422).send({error: "Error in getting course details"});
+        res.status(422).send({ error: "Error in getting course details" });
     }
 }
