c4d299452476e4d8901a4d8eaa765ad07e0ffdf9
Mukund-Khunt <55125414+Vivekkheni@users.noreply.github.com>
2023-01-28 11:21:32 +0530

    add .env file script folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 stat build || mkdir build
 # Archive artifacts
 # cd build
-zip build/jkr.zip -r package.json src .platform sample.env .npmrc signImages startindex.js
+zip build/jkr.zip -r package.json src .platform sample.env .npmrc signImages startindex.js .env
