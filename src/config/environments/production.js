ea466d842da079726648ed95ffc70e05abb6d186
kevalbhalala <65419008+kevalbhalala@users.noreply.github.com>
2023-04-11 17:00:33 +0530

    credentails changes

--- src/config/environments/production.js
+++ src/config/environments/production.js
@@ -1,7 +1,7 @@
 module.exports = {
     host: "localhost",
     mongoUrl: "mongodb+srv://nodejs:9nuDETxvDexCJYZY@nodejs.nqle14b.mongodb.net/jkr_production?retryWrites=true&w=majority",
-    fcmKey: "AAAAILEao48:APA91bERTSKeINuweOhf6Gis3z2nSNtvQ4RRyxIxmgz5m56TeXOs8qf16fXSrap0ee27Ar5VAzxjA7xvlH6emgyWkFyvB3rBg1uxic5XKswCJKmrhG0TeizPUhjjkfb9oPjneczX7Xkm",
+    fcmKey: "AAAAILEao48:APA91bHCFtkivl-RHkyvVdLz-hd4kdRYqFm_sEUgyZA3Yo-0gGZdc5Okfn7GMgP7p7CdVdPDs_XjST8jeqeadp6Np1ZYJ_3osHDRDsgbd0R_pQ-RhvV5-yymlt3Wj-CKGUNUY4KyxqP9",
     logLevel: "debug",
     secret: "superSuperSecret",
     backend_URL: "http://jkr-production.ap-south-1.elasticbeanstalk.com"
